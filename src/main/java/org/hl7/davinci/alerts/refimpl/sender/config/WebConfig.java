ed95141b45e74431fbdf0c9ad2ce66d5cd8dcd4e
a.kravs <a.kravs@gmail.com>
2019-10-24 17:10:45 +0300

    configuration extracted

--- src/main/java/org/hl7/davinci/alerts/refimpl/sender/config/WebConfig.java
+++ src/main/java/org/hl7/davinci/alerts/refimpl/sender/config/WebConfig.java
@@ -1,26 +1,25 @@
 package org.hl7.davinci.alerts.refimpl.sender.config;
 
+import com.healthlx.smartonfhir.config.EnableSmartOnFhir;
 import org.hl7.davinci.alerts.refimpl.sender.support.CurrentContextArgumentResolver;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
+@EnableSmartOnFhir
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Autowired
-    private CurrentContextArgumentResolver currentContextArgumentResolver;
+  @Autowired
+  private CurrentContextArgumentResolver currentContextArgumentResolver;
 
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        argumentResolvers.add(currentContextArgumentResolver);
-    }
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+    argumentResolvers.add(currentContextArgumentResolver);
+  }
 
 }
 
